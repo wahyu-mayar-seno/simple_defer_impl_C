a3c7d4eab00b5ffdd4e17d2171716a716055e91d
wahyu-mayar <wahyumayar111@gmail.com>
2025-10-25 20:35:11 +0700

    change malloc into static allocate in the stact because defer is static not runtime

--- src/defer.c
+++ src/defer.c
@@ -1,2 +1,32 @@
-#define DEFER_C_IMPLEMENTATION
 #include "defer.h"
+#include <stdio.h>
+#include <stdlib.h>
+void defer_push(Defer_Table *d, DeferFn fn, void *data) {
+  if (!d) {
+    return;
+  }
+  if (d->cap <= d->count) {
+    fprintf(stderr, "no slot any more\n");
+    exit(-1);
+    return;
+  }
+  d->task[d->count].func = fn;
+  d->task[d->count].data = data;
+  d->count++;
+}
+void defer_exec(Defer_Table *d) {
+  if (!d) {
+    fprintf(stderr, "invalid pointer to table\n");
+  }
+  if (d->count < 1) {
+    fprintf(stderr, "no task to exec\n");
+    exit(-1);
+    return;
+  }
+  int size = d->count;
+  while (size > 0) {
+    size--;
+    d->task[size].func(d->task[size].data);
+  }
+  d->count = 0;
+}
--- src/main.c
+++ src/main.c
@@ -11,21 +11,30 @@ void *dmalloc(size_t s) {
 }
 void dfree(void *fj) {
   free_count++;
+  printf("free %zu\n", free_count);
   free(fj);
 }
+void ff(void *a) { printf("aha\n"); }
 int main(void) {
-  Defer_Table t;
-  defer_init(&t, 1024);
+  Defer_Item _task[10];
+  Defer_Table t = {.task = _task, .count = 0, .cap = 10};
   void *a = dmalloc(256);
-  void *b = dmalloc(256);
-  void *c = dmalloc(256);
-  void *d = dmalloc(256);
-  void *e = dmalloc(256);
   defer_push(&t, dfree, a);
+  void *b = dmalloc(256);
   defer_push(&t, dfree, b);
+  void *c = dmalloc(256);
   defer_push(&t, dfree, c);
+  void *d = dmalloc(256);
   defer_push(&t, dfree, d);
+  void *e = dmalloc(256);
   defer_push(&t, dfree, e);
+  defer_push(&t, ff, 0);
+  defer_push(&t, ff, 0);
+  defer_push(&t, ff, 0);
+  defer_push(&t, ff, 0);
+  defer_push(&t, ff, 0);
+  // defer_push(&t, ff, 0);// this call will exit because task is full
+
   defer_exec(&t);
   printf("malloc : %zu\n free : %zu\n", malloc_count, free_count);
 
